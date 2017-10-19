4f3d0836872a4a54795f65685ee6d0ad942521b9
Luca Boccassi <luca.boccassi@gmail.com>
2017-10-19 11:18:25 +0100

    Problem: build broken on mips64el
    Solution: add definitions to prelude

--- src/prelude.h
+++ src/prelude.h
@@ -91,7 +91,8 @@
 
 #if (defined (__64BIT__) || defined (__x86_64__) || defined (__AARCH64EL__) \
     || defined (__PPC64__) || defined (__powerpc64__) || defined (__ppc64__) \
-    || defined (__s390x__) || (defined (__sparc__) && defined (__arch64__)))
+    || defined (__s390x__) || (defined (__sparc__) && defined (__arch64__)) \
+    || (defined (__mips__) && defined (_MIPSEL) && _MIPS_SIM == _ABI64))
 #    define __IS_64BIT__                /*  May have 64-bit OS/compiler      */
 #else
 #    define __IS_32BIT__                /*  Else assume 32-bit OS/compiler   */
