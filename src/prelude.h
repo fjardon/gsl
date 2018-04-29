e80cd452a4b602553063a62046286a24abd6d722
Luca Boccassi <luca.boccassi@gmail.com>
2018-04-29 16:11:45 +0100

    Problem: build fails on RISC-V
    Solution: add preprocessor checks for 64 bit

--- src/prelude.h
+++ src/prelude.h
@@ -93,6 +93,7 @@
     || defined (__PPC64__) || defined (__powerpc64__) || defined (__ppc64__) \
     || defined (__s390x__) || (defined (__sparc__) && defined (__arch64__)) \
     || defined (__ia64) || defined (__itanium__) || defined (_M_IA64) \
+    || defined (__riscv64) || (defined (__riscv_xlen) && __riscv_xlen == 64) \
     || (defined (__mips__) && defined (_MIPSEL) && _MIPS_SIM == _ABI64))
 #    define __IS_64BIT__                /*  May have 64-bit OS/compiler      */
 #else
